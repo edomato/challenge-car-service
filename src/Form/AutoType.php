3508c609aad076c5bbf501528894e4e3830ecebc
Ernesto Domato <edomato@gmail.com>
2022-05-03 16:12:47 -0300

    Added forms for Propietario and Auto

---
+++ src/Form/AutoType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Auto;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AutoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('marca')
+            ->add('modelo')
+            ->add('anio')
+            ->add('patente')
+            ->add('color')
+            ->add('propietario')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Auto::class,
+        ]);
+    }
+}
